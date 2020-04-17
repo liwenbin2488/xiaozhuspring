2a3d86406b5b60593fd6de0277078ae7e0c23129
liwenbin <musonguihong@163.com>
2020-04-17 21:04:50 +0800

    second commit

---
+++ SpringTest/src/main/java/com/lwb/SpringTest/App.java
@@ -0,0 +1,20 @@
+package com.lwb.SpringTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Hello world!
+ *
+ */
+@ComponentScan({"com.lwb.Controller"})
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
